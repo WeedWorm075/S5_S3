ceda8bf8c07bf85c3e31c3f5fdd867743e8fdead
WeedWorm075 <119393204+WeedWorm075@users.noreply.github.com>
2023-12-12 13:32:34 +0100

    begin

---
+++ src/java/dao/Connexion_projet.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package connexion;
+
+/**
+ *
+ * @author Hasinjo
+ */
+public class Connexion_projet extends Connexion{
+    
+    public Connexion_projet(String base, String user, String password, String database) {
+        super(base, user, password, database);
+    }
+
+    public Connexion_projet() {
+        setBase("postgresql");
+        setUser("postgres");
+        setPassword("root");
+        setDatabase("test");
+    }
+    
+    
+}
